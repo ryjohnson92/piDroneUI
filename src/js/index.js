738e1c11b45a9b80e30524f6f2949e7f7db09ba1
Ryan Johnson <ryanjohnson@Ryans-MacBook-Pro.local>
2020-06-21 21:15:36 -0400

    created react app

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class Index extends Component {
+    state = {  }
+    render() { 
+        return ( <h2>Welcome to react!</h2> );
+    }
+}
+ 
+export default Index;
